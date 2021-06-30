efb1f35e6851270d712b4100ff67f3c358b2d8ce
Peter Torok <peter.toeroek@rms.com>
2021-06-30 15:15:14 +0100

    Adding more tests

--- src/main/kotlin/iterators/Fibonacci.kt
+++ src/main/kotlin/iterators/Fibonacci.kt
@@ -5,11 +5,20 @@ open class Fibonacci(private val limit: Int) : Iterator<Int> {
     var second = 1
     var nextTerm = 1
 
+    var called = 0
+
     override fun hasNext(): Boolean {
         return nextTerm < limit
     }
 
     override fun next(): Int {
+        if(++called == 1) {
+            return first
+        }
+        if(called == 2) {
+            return second
+        }
+
         first = second
         second = nextTerm
         nextTerm = first + second
